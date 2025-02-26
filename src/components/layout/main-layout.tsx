48f61dbbf6d776fecc79b5510addcb9c18d3bde9
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-26 00:32:50 -0500

    ✨ fetch chats data in MainLayout and pass to Sidebar component

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,16 +1,24 @@
 import { Navbar } from './navbar';
 import { Sidebar } from './sidebar';
+import { useQuery } from 'react-query';
+import { apiRequest } from '@/lib/queryClient';
 
 interface MainLayoutProps {
     children: React.ReactNode;
 }
 
 export function MainLayout({ children }: MainLayoutProps) {
+    const chatsQuery = useQuery({
+        queryKey: ['chats'],
+        queryFn: () =>
+            apiRequest('GET', '/api/chats').then((res) => res.json()),
+    });
+
     return (
         <div className="flex h-screen flex-col">
             <Navbar />
             <div className="flex flex-1 overflow-hidden">
-                <Sidebar />
+                <Sidebar chats={chatsQuery.data || []} />
                 <main className="flex-1 overflow-auto">{children}</main>
             </div>
         </div>
