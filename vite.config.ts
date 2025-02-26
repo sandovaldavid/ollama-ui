89f3812215b5d553b86f01694578cdaa21f666b8
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-25 22:28:59 -0500

    🔧 update Vite configuration to add path aliases and set build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
+            '@': path.resolve(__dirname, './src'),
             '@components': path.resolve(__dirname, './src/components'),
-            '@shared': path.resolve(__dirname, './src/shared'),
-            '@server': path.resolve(__dirname, './src/server'),
             '@hooks': path.resolve(__dirname, './src/hooks'),
             '@lib': path.resolve(__dirname, './src/lib'),
+            '@shared': path.resolve(__dirname, './shared'),
+            '@server': path.resolve(__dirname, './server'),
         },
+    },
+    build: {
+        outDir: path.resolve(__dirname, 'dist/public'),
+        emptyOutDir: true,
     },
 });
